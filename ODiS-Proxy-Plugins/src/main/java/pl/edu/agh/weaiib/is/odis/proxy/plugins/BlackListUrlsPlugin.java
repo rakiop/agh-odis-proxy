bcc92a347bc4d908fd7c9a38f07ff32b4f79752d
rakiop <rakiop@o2.pl>
2016-03-25 20:39:02 +0100

    Filter out also subdomains

--- ODiS-Proxy-Plugins/src/main/java/pl/edu/agh/weaiib/is/odis/proxy/plugins/BlackListUrlsPlugin.java
+++ ODiS-Proxy-Plugins/src/main/java/pl/edu/agh/weaiib/is/odis/proxy/plugins/BlackListUrlsPlugin.java
@@ -3,21 +3,28 @@ package pl.edu.agh.weaiib.is.odis.proxy.plugins;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.HttpObject;
 import io.netty.handler.codec.http.HttpRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pl.edu.agh.weaiib.is.odis.proxy.helpers.URIHelper;
+
 import java.net.InetAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import pl.edu.agh.weaiib.is.odis.proxy.helpers.URIHelper;
+import java.util.regex.Pattern;
 
 public class BlackListUrlsPlugin extends ODiSHttpFilter{
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BlackListUrlsPlugin.class);
 
-    private List<String> urls = new ArrayList<String>();
+    private static final Pattern IPv4Pattern = Pattern.compile("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}");
+    private static final Pattern IPv6Patternv1 = Pattern.compile("[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}");
+    private static final Pattern IPv6Patternv2 = Pattern.compile("[0-9a-fA-F]{1,4}\\.[0-9a-fA-F]{1,4}\\.[0-9a-fA-F]{1,4}\\.[0-9a-fA-F]{1,4}\\.[0-9a-fA-F]{1,4}\\.[0-9a-fA-F]{1,4}\\.[0-9a-fA-F]{1,4}\\.[0-9a-fA-F]{1,4}");
+
+    private List<String> fullDomains = new ArrayList<String>();
+    private List<String> subdomainsSufix = new ArrayList<String>();
 
     @Override
     public void init() {
@@ -28,8 +35,9 @@ public class BlackListUrlsPlugin extends ODiSHttpFilter{
                 String trimmedUrl = url.trim().toLowerCase();
                 if(trimmedUrl.startsWith("www."))
                     trimmedUrl = trimmedUrl.substring(4);
-                if(!trimmedUrl.isEmpty() && !urls.contains(trimmedUrl)){
-                    urls.add(trimmedUrl);
+                if(!trimmedUrl.isEmpty() && !fullDomains.contains(trimmedUrl)){
+                    fullDomains.add(trimmedUrl);
+                    subdomainsSufix.add("." + trimmedUrl);
                 }
             }
         }
@@ -37,26 +45,46 @@ public class BlackListUrlsPlugin extends ODiSHttpFilter{
 
     @Override
     public boolean testHttpRequest(HttpRequest originalRequest, ChannelHandlerContext ctx) {
-        String url = originalRequest.getUri();
+        String uri = originalRequest.getUri();
+        String host = getHost(uri);
+
+        if(fullDomains.contains(host))
+            return false;
+
+        for(String sub : subdomainsSufix){
+            if(host.endsWith(sub))
+                return false;
+        }
+
+        return true;
+    }
+
+    private String getHost(String url){
+        String host = "";
         try {
             URI uri = URIHelper.getURIFromUrl(url);
-            String host = uri.getHost();
+            host = uri.getHost();
+            if(host == null)
+                host = uri.getSchemeSpecificPart();
             if(host.startsWith("www."))
                 host = host.substring(4);
 
             try {
-                InetAddress addr = InetAddress.getByName(host);
-                host = addr.getHostName();
+                if(IPv4Pattern.matcher(host).find() ||
+                        IPv6Patternv1.matcher(host).find() ||
+                        IPv6Patternv2.matcher(host).find()){
+                    InetAddress inetAddress = InetAddress.getByName(host);
+                    host = inetAddress.getHostName();
+                }
             } catch (UnknownHostException e) {
                 LOGGER.warn(String.format("Could not resolve host with InetAddress: %s", e.getMessage()));
             }
 
-            return !urls.contains(host);
+            return host;
         } catch (URISyntaxException e) {
             LOGGER.warn(String.format("Could not resolve host to URI: %s", e.getMessage()));
         }
-
-        return true;
+        return host;
     }
 
     @Override
---
+++ ODiS-Proxy-Plugins/src/test/java/pl/edu/agh/weaiib/is/odis/proxy/plugins/BlackListUrlsPluginTest.java
@@ -0,0 +1,109 @@
+package pl.edu.agh.weaiib.is.odis.proxy.plugins;
+
+
+import io.netty.handler.codec.http.HttpRequest;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.TestCase.assertFalse;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class BlackListUrlsPluginTest {
+
+    @Test
+    public void dropDomainIfOnList(){
+        BlackListUrlsPlugin list = initializeBlackListUrls("google.pl");
+        HttpRequest request = mock(HttpRequest.class);
+        when(request.getUri()).thenReturn("http://google.pl/?q=question");
+
+        assertFalse(list.testHttpRequest(request, null));
+    }
+
+    @Test
+    public void passDomainIfNotOnList(){
+        BlackListUrlsPlugin list = initializeBlackListUrls("google.pl");
+        HttpRequest request = mock(HttpRequest.class);
+        when(request.getUri()).thenReturn("http://google.com/?q=question");
+
+        assertTrue(list.testHttpRequest(request, null));
+    }
+
+    @Test
+    public void dropDomainIfUrlIsIpAndDomainOnList(){
+        BlackListUrlsPlugin list = initializeBlackListUrls("coachingsport.pl");
+        HttpRequest request = mock(HttpRequest.class);
+        when(request.getUri()).thenReturn("http://5.133.13.87");
+
+        assertFalse(list.testHttpRequest(request, null));
+    }
+
+    @Test
+    public void dropIfIsSubdomain(){
+        BlackListUrlsPlugin list = initializeBlackListUrls("kwejk.pl");
+        HttpRequest request = mock(HttpRequest.class);
+        when(request.getUri()).thenReturn("http://m.kwejk.pl");
+
+        assertFalse(list.testHttpRequest(request, null));
+    }
+
+    @Test
+    public void dropWWWPrefixFromBlakListWorks(){
+        BlackListUrlsPlugin list = initializeBlackListUrls("www.google.pl");
+        HttpRequest request = mock(HttpRequest.class);
+        when(request.getUri()).thenReturn("http://google.pl");
+
+        assertFalse(list.testHttpRequest(request, null));
+    }
+
+    @Test
+    public void dropWWWPrefixedUrl(){
+        BlackListUrlsPlugin list = initializeBlackListUrls("google.pl");
+        HttpRequest request = mock(HttpRequest.class);
+        when(request.getUri()).thenReturn("http://www.google.pl");
+
+        assertFalse(list.testHttpRequest(request, null));
+    }
+
+
+    @Test
+    public void responseTestAlwaysTrue(){
+        BlackListUrlsPlugin list = initializeBlackListUrls("kwejk.pl");
+        assertTrue(list.testHttpResponse(null, null, null));
+    }
+
+    @Test
+    public void unknowHostIpNoError(){
+        BlackListUrlsPlugin list = initializeBlackListUrls("google.pl");
+        HttpRequest request = mock(HttpRequest.class);
+        when(request.getUri()).thenReturn("http://0000:0000:0000:0000:0000:0000:0000:0000%5");
+
+        assertTrue(list.testHttpRequest(request, null));
+    }
+
+    @Test
+    public void wrongUriProtection(){
+        BlackListUrlsPlugin list = initializeBlackListUrls("google.pl");
+        HttpRequest request = mock(HttpRequest.class);
+        when(request.getUri()).thenReturn("http://sadih$@%@$@#");
+
+        assertTrue(list.testHttpRequest(request, null));
+    }
+
+
+
+
+    private BlackListUrlsPlugin initializeBlackListUrls(String domainList){
+        BlackListUrlsPlugin list = new BlackListUrlsPlugin();
+        Map<String, String> params = new HashMap<>();
+        params.put("list",domainList);
+        list.setParameters(params);
+        list.init();
+
+        return list;
+    }
+
+}
