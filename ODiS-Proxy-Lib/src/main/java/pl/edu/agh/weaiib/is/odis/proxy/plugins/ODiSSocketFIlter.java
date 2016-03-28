d29bc66d607dfdd073a67d799abe06cb3bf6fd74
rakiop <rakiop@o2.pl>
2016-03-28 12:57:09 +0200

    Remove file

--- ODiS-Proxy-Lib/src/main/java/pl/edu/agh/weaiib/is/odis/proxy/plugins/ODiSSocketFIlter.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.agh.weaiib.is.odis.proxy.plugins;
-
-import java.net.Socket;
-
-public abstract class ODiSSocketFIlter extends Filter {
-
-    public abstract boolean testSocketRequest(Socket client);
-
-}
